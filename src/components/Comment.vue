e3364d7965f9ccdeb9d21d19c55cf2e43bbd3b5f
Adam Nowacki <ad.nowacki@gmail.com>
2025-12-02 19:08:39 +0100

    Add handlers

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -3,8 +3,6 @@
         <div>{{ comment.author }}</div>
         <div>{{ comment.content }}</div>
         <div>Data: {{ formatDate(comment.created_at) }}</div>
-        <!-- <span v-for="(c, r) in  comment.reactions">{{ r }} {{ c }}</span> -->
-
         <Reaction @click="clickHandler" class="mt-3" :comment="comment" />
     </div>
 </template>
--- src/components/Reaction.vue
+++ src/components/Reaction.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="flex gap-2">
-        <span @click="emit('click', reaction)" v-for="reaction in reactions" :key="reaction" class="cursor-pointer">
-            {{ reaction }} {{ comment.reactions[reaction] }}
+        <span @click="clickHandler(reaction)" v-for="reaction in reactions" :key="reaction" class="cursor-pointer">
+            {{ reaction }} {{ calculatedReaction(comment.reactions, reaction) }}
         </span>
     </div>
 </template>
 <script setup>
+import { ref } from 'vue';
+
 const reactions = ["❤️", "😂", "👍", "😮", "👎"];
 const emit = defineEmits(['click']);
+
+const myReaction = ref(null);
 
 const props = defineProps({
     comment: {
@@ -15,4 +19,14 @@ const props = defineProps({
         default: () => ({})
     }
 });
+
+const clickHandler = (reaction) => {
+    myReaction.value = myReaction.value === reaction ? null : reaction;
+}
+
+const calculatedReaction = (reactions, reaction) => {
+    if (!myReaction.value || reaction !== myReaction.value) return reactions[reaction];
+
+    return reactions[reaction] + 1;
+}
 </script>
